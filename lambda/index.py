2a431e379ccc911c651b89e572d17fc8c17b10a0
create-alt <134464021+create-alt@users.noreply.github.com>
2025-04-30 10:32:16 +0900

    Update index.py

--- lambda/index.py
+++ lambda/index.py
@@ -18,7 +18,8 @@ def extract_region_from_arn(arn):
 bedrock_client = None
 
 # モデルID
-MODEL_ID = os.environ.get("MODEL_ID", "us.amazon.nova-lite-v1:0")
+# MODEL_ID = os.environ.get("MODEL_ID", "us.amazon.nova-lite-v1:0")
+MODEL_ID = "https://f747-35-221-238-30.ngrok-free.app"
 
 def lambda_handler(event, context):
     try:
